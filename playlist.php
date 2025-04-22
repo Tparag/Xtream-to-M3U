ec6d3a5f6cbc6fec11afd42e802a70936d28abee
Parag <84992283+Tparag@users.noreply.github.com>
2025-04-22 23:54:31 +0530

    Update playlist.php

--- playlist.php
+++ playlist.php
@@ -120,9 +120,8 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-header("Content-Type: application/json");
-//header('Content-Type: audio/x-mpegurl');
-//header('Content-Disposition: inline; filename="playlist.m3u"');
+header('Content-Type: audio/x-mpegurl');
+header('Content-Disposition: inline; filename="playlist.m3u"');
 
 echo $m3uContent;
 exit();
